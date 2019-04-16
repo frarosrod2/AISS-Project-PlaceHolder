1223681f65cfa54b755588ff201ef054859f3aba
frarosrod2 <48064334+frarosrod2@users.noreply.github.com>
2019-04-16 18:50:04 +0200

    Update TrelloResource.java

--- PlaceHolder-AISS/src/main/java/aiss/resources/TrelloResource.java
+++ PlaceHolder-AISS/src/main/java/aiss/resources/TrelloResource.java
@@ -1,5 +1,24 @@
 package aiss.resources;
 
+import java.util.logging.Logger;
+
+import aiss.trello.Board;
+
 public class TrelloResource {
+
+	private static final Logger log = Logger.getLogger(TrelloResource.class.getName());
+	
+	private final String usuario;
+	private final String clave;
+	private final String base_url = "https://developers.trello.com/";
+
+	public TrelloResource(String usuario, String clave) {
+		this.usuario = usuario;
+		this.clave = clave;
+	}
+
+	public boolean addBoard(String name, Board datos) {
+		String
+	}
 
 }
