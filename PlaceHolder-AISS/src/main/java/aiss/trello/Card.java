c75d80c480e3786659c887e273570b1a68b672cf
frarosrod2 <48064334+frarosrod2@users.noreply.github.com>
2019-04-16 18:48:13 +0200

    Update Card.java

--- PlaceHolder-AISS/src/main/java/aiss/trello/Card.java
+++ PlaceHolder-AISS/src/main/java/aiss/trello/Card.java
@@ -1,5 +1,53 @@
 package aiss.trello;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+	@JsonInclude(JsonInclude.Include.NON_NULL)
+	@JsonPropertyOrder({
+	    "name",
+	    "id",
+	    "idList"
+	})
 public class Card {
+	
+		@JsonProperty("name")
+	    private String name;
+	    @JsonProperty("id")
+	    private Integer id;
+	    @JsonProperty("idList")
+	    private Integer idList;
+	    
+	    @JsonProperty("name")
+	    public String getName() {
+	        return name;
+	    }
+
+	    @JsonProperty("name")
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+
+	    @JsonProperty("id")
+	    public Integer getId() {
+	        return id;
+	    }
+
+	    @JsonProperty("id")
+	    public void setId(Integer id) {
+	        this.id = id;
+	    }
+	    
+	    @JsonProperty("idList")
+	    public Integer getIdList() {
+	        return idList;
+	    }
+
+	    @JsonProperty("idList")
+	    public void setIdList(Integer idList) {
+	        this.idList = idList;
+	    }
 
 }
+
