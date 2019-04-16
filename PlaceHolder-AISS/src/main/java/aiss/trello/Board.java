838757184369217eeabaf39a8f005c1bdf1d1f51
frarosrod2 <48064334+frarosrod2@users.noreply.github.com>
2019-04-16 18:45:37 +0200

    Update Board.java

--- PlaceHolder-AISS/src/main/java/aiss/trello/Board.java
+++ PlaceHolder-AISS/src/main/java/aiss/trello/Board.java
@@ -1,5 +1,46 @@
 package aiss.trello;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+	@JsonPropertyOrder({
+	    "name",
+	    "id"
+	})
+
 public class Board {
 
+	@JsonProperty("name")
+    private String name;
+    @JsonProperty("id")
+    private Integer id;
+    
+    
+
+	public Board(String name) {
+		super();
+		this.name = name;
+	}
+
+	@JsonProperty("href")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("href")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("id")
+    public Integer getId() {
+        return id;
+    }
+
+    @JsonProperty("id")
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
